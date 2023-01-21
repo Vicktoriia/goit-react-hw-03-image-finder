dc0392805b874252c6a17d79bd857f3019c90a69
Viktoriia Aleksieieva <105809872+Vicktoriia@users.noreply.github.com>
2023-01-21 14:37:40 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,35 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, StyledModal } from './Modal.styled';
 
 const ModalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscKeydown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscKeydown);
-  }
-
-  handleEscKeydown = e => {
-    if (e.code === 'Escape') {
-      this.props.activeModal();
-    }
-  };
-
-  handleOverlayClick = e => {
+const Modal = ({ activeModal, children }) => {
+  const handleOverlayClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.activeModal();
+      activeModal();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleOverlayClick}>
-        <StyledModal>{this.props.children}</StyledModal>
-      </Overlay>,
-      ModalRoot
-    );
-  }
-}
+  useEffect(() => {
+    const handleEscKeydown = e => {
+      if (e.code === 'Escape') {
+        activeModal();
+      }
+    };
+
+    window.addEventListener('keydown', handleEscKeydown);
+    return () => {
+      window.removeEventListener('keydown', handleEscKeydown);
+    };
+  }, [activeModal]);
+
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <StyledModal>{children}</StyledModal>
+    </Overlay>,
+    ModalRoot
+  );
+};
 
 export default Modal;
